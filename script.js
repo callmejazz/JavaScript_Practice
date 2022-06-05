737fd8ee58865f6f3b9a6fe93ee0e11e30b2bfe9
callmejazz <jmeade4347@gmail.com>
2022-06-05 17:30:41 -0400

    Challenge two

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
 // BMI (28.3) is higher than John's (23.9)!"
 
-
+// Challenge Two
 const johnMass = 78;
 const johnHeight = 1.69;
 const markMass = 92;
