dfa01abf692259679b4f784f5efa1c1f850849a5
callmejazz <jmeade4347@gmail.com>
2022-06-05 12:36:25 -0400

    ChallengeOne BMI

---
+++ ChallengeOne/script.js
@@ -0,0 +1,33 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+// GOOD LUCK �
+
+//Psudo Code/////////////////////////////////////////////////////////////////////////////
+// Make a variable(const) for John for his hight and weight
+//Make a variable for Mark(const) for his hieght and weight
+// calculate the formula for both people
+//make a conditional state to check to see which is higher
+
+const johnMass = 250;
+const johnHeight = 67;
+const markMass = 190;
+const markHeight = 70;
+
+const BMIJohn = johnMass / johnHeight ** 2;
+const BMIMark = markMass / markHeight ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(markHigherBMI)
